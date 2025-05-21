36ac728d346db26fc7582f2b3d47defc15ec2ca9
BinayakShome <binayakshome3@gmail.com>
2025-05-21 21:36:10 +0530

    ...

--- src/Arrays/Part1/IsSorted.java
+++ src/Arrays/Part1/IsSorted.java
@@ -1,8 +1,9 @@
 package Arrays.Part1;
 
 public class IsSorted {
-    static boolean isSorted(int arr[], int n)
+    static boolean isSorted(int arr[])
     {
+        int n = arr.length;
        for (int i = 1; i<n; i++)
        {
             if(arr[i]<arr[i-1])
@@ -17,8 +18,7 @@ public class IsSorted {
     {
         int arr1[] = {3,4,5,1,2};
         int arr2[] = {2,5,676,21,23,56,76,23,456,76};
-        int n1 = arr1.length, n2 = arr2.length;
-        System.out.println(isSorted(arr1,n1));
-        System.out.println(isSorted(arr2, n2));
+        System.out.println(isSorted(arr1));
+        System.out.println(isSorted(arr2));
     }
 }
