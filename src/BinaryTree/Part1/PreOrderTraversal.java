e704cf3ae2d3d07f06b7a1056e12268d6e71cfb9
BinayakShome <binayakshome3@gmail.com>
2025-06-08 10:04:47 +0530

    Binary Tree

--- src/BinaryTree/Part1/PreOrderTraversal.java
+++ src/BinaryTree/Part1/PreOrderTraversal.java
@@ -1,5 +1,8 @@
 package BinaryTree.Part1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 //class Node
 //{
 //    int data;
@@ -14,8 +17,29 @@ package BinaryTree.Part1;
 //    }
 //}
 public class PreOrderTraversal {
+    static List<Integer> preorder(Node root)
+    {
+        List<Integer> result = new ArrayList<>();
+        preOrderHelper(root,result);
+        return result;
+    }
+
+    static void preOrderHelper(Node node, List<Integer> result)
+    {
+        if(node == null) return;
+
+        result.add(node.data);
+        preOrderHelper(node.left, result);
+        preOrderHelper(node.right, result);
+    }
     public static void main(String []args)
     {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
 
+        System.out.println(preorder(root));
     }
 }
