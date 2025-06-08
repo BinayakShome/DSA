63d7e16669b7dc6ad5fb4c7c69c82ea7583bc97d
BinayakShome <binayakshome3@gmail.com>
2025-06-08 09:44:26 +0530

    Binary Tree

---
+++ src/BinaryTree/Part1/Node.java
@@ -0,0 +1,15 @@
+package BinaryTree.Part1;
+
+class Node
+{
+    int data;
+    Node left;
+    Node right;
+
+    public Node(int key)
+    {
+        data = key;
+        left = null;
+        right = null;
+    }
+}
--- src/BinaryTree/Part1/PreOrderTraversal.java
+++ src/BinaryTree/Part1/PreOrderTraversal.java
@@ -13,13 +13,9 @@ package BinaryTree.Part1;
 //        right = null;
 //    }
 //}
-
-public class TreeRepresentation {
+public class PreOrderTraversal {
     public static void main(String []args)
     {
-        Node root = new Node(1);
-        root.left = new Node(2);
-        root.right = new Node(3);
-        root.left.right = new Node(4);
+
     }
 }
