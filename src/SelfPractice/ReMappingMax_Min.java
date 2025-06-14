fbcc046702aac6b5fd94aa6e55008946b6862ab5
BinayakShome <binayakshome3@gmail.com>
2025-06-14 09:16:24 +0530

    self practice

---
+++ src/SelfPractice/ReMappingMax_Min.java
@@ -0,0 +1,27 @@
+package SelfPractice;
+
+public class ReMappingMax_Min {
+    static int minMaxDifference(int num)
+    {
+        String s = Integer.toString(num);
+        char firstNon9 = 0;
+        char firstNon0 = 0;
+
+        for(char c : s.toCharArray())
+        {
+            if(firstNon9 == 0 && c >= '0' && c <= '8') firstNon9 = c;
+            if(firstNon0 == 0 && c >= '1' && c <= '9') firstNon0 = c;
+        }
+
+        String maxStr = (firstNon9 != 0) ? s.replace(firstNon9, '9') : s;
+        String minStr = (firstNon0 != 0) ? s.replace(firstNon0, '0') : s;
+
+        return Integer.parseInt(maxStr) - Integer.parseInt(minStr);
+    }
+    public static void main(String []args)
+    {
+        int num1 = 11891;
+        int num2 = 90;
+        System.out.println(minMaxDifference(num1));
+    }
+}
