6a393124300e40643a418f4d494edfa6e380afbc
BinayakShome <binayakshome3@gmail.com>
2025-06-16 11:59:45 +0530

    Two Pointers practice

---
+++ src/SelfPractice/LongestPalindrome.java
@@ -0,0 +1,12 @@
+package SelfPractice;
+
+public class LongestPalindrome {
+//    static int longestPalindrome(String s)
+//    {
+//
+//    }
+    public static void main(String []args)
+    {
+        String s = "abccccdd";
+    }
+}
