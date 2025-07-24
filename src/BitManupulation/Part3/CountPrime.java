c7622f90f158a4f024b7868b76c1ee619087ffa5
BinayakShome <binayakshome3@gmail.com>
2025-07-24 15:44:04 +0530

    Bit Manipulation

---
+++ src/BitManupulation/Part3/CountPrime.java
@@ -0,0 +1,34 @@
+package BitManupulation.Part3;
+
+import java.util.Arrays;
+
+public class CountPrime {
+    static int countPrimes(int n)
+    {
+        if(n <= 2) return 0;
+        boolean[] isPrime = new boolean[n];
+        Arrays.fill(isPrime, true);
+        isPrime[0] = isPrime[1] = false;
+
+        for(int i = 2; i * i < n; i++)
+        {
+            if(isPrime[i])
+            {
+                for (int j = i * i; j < n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+        int count = 0;
+        for (boolean prime : isPrime) {
+            if (prime) count++;
+        }
+
+        return count;
+    }
+    public static void main(String []args)
+    {
+        int n = 10;
+        System.out.println(countPrimes(n));
+    }
+}
