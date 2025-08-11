3fbd8d3174044516c158587f88a26de050df1fbe
BinayakShome <binayakshome3@gmail.com>
2025-08-11 23:51:37 +0530

    LeetCode problem

---
+++ src/LeetCodeProblems/AddBinary.java
@@ -0,0 +1,34 @@
+package LeetCodeProblems;
+
+public class AddBinary {
+    static String addbinary(String a, String b)
+    {
+                char[] charArrA = a.toCharArray();
+                char[] charArrB = b.toCharArray();
+                StringBuilder sb = new StringBuilder();
+
+                int i = charArrA.length - 1, j = charArrB.length - 1;
+                int carry = 0;
+                while (i >= 0 || j >= 0 || carry > 0) {
+                    int aNum = i >= 0 ? charArrA[i] - '0' : 0;
+                    int bNum = j >= 0 ? charArrB[j] - '0' : 0;
+
+                    int sum = aNum + bNum + carry;
+                    int digit = sum % 2;
+                    carry = sum / 2;
+                    i--;
+                    j--;
+
+                    sb.append((char)(digit + '0'));
+                }
+
+                return sb.reverse().toString();
+    }
+    public static void main(String []args)
+    {
+        String a = "11";
+        String b = "1";
+
+        System.out.println(addbinary(a,b));
+    }
+}
