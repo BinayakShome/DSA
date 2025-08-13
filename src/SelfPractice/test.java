3440b55b4f135ecfe0f0555bc608293bba01ee0f
BinayakShome <binayakshome3@gmail.com>
2025-08-13 19:45:27 +0530

    LeetCode problem

--- src/SelfPractice/test.java
+++ src/SelfPractice/test.java
@@ -3,8 +3,6 @@ package SelfPractice;
 public class test {
     public static void main(String []args)
     {
-        int num = 32;
-        String binary = Integer.toBinaryString(num);
-        System.out.println(binary);
+        System.out.println("Hello World");
     }
 }
