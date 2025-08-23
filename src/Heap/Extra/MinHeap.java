4e1ca8c5d977270d6e2b86387dc478935b928a26
BinayakShome <binayakshome3@gmail.com>
2025-08-23 12:07:11 +0530

    Heap

---
+++ src/Heap/Extra/MinHeap.java
@@ -0,0 +1,51 @@
+package Heap.Extra;
+
+public class MinHeap {
+    static void swap(int[] arr, int i, int j)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    static void heapify(int []arr, int i, int n)
+    {
+        int root = i;
+        int left = 2*i + 1;
+        int right = 2*i + 2;
+
+        if (left < n && arr[left] < arr[root]) root = left;
+        if (right < n && arr[right] < arr[root]) root = right;
+        if (root != i) {
+            swap(arr, i, root);
+            heapify(arr, n, root);
+        }
+    }
+
+    static void buildHeap(int []arr)
+    {
+        int n = arr.length;
+        for (int i = n/2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+    }
+
+    static void print(int []arr)
+    {
+        int n = arr.length;
+        for(int i = 0; i < n; i++)
+        {
+            System.out.print(arr[i] + "  ");
+        }
+    }
+    public static void main(String []args)
+    {
+        int []arr = {20, 10, 17, 30, 40};
+        System.out.print("Before: ");
+        print(arr);
+        buildHeap(arr);
+        System.out.println();
+        System.out.print("After: ");
+        print(arr);
+    }
+}
