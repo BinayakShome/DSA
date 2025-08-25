45a0273cda0c80e6ea2b21523ef1d572375a7bfe
BinayakShome <binayakshome3@gmail.com>
2025-08-26 00:43:44 +0530

    Arrays

---
+++ src/Arrays/Extra/Permutations.java
@@ -0,0 +1,40 @@
+package Arrays.Extra;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Permutations {
+    static List<List<Integer>> permut(int []arr)
+    {
+        List<List<Integer>> ans = new ArrayList<>();
+        backtrack(arr, new ArrayList<>(), new boolean[arr.length], ans);
+        return ans;
+    }
+
+    static void backtrack(int []nums, List<Integer> curr, boolean[] used, List<List<Integer>> result)
+    {
+        if(curr.size() == nums.length)
+        {
+            result.add(new ArrayList<>(curr));
+            return;
+        }
+
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(used[i]) continue;
+
+            curr.add(nums[i]);
+            used[i] = true;
+
+            backtrack(nums, curr, used, result);
+
+            curr.remove(curr.size() - 1);
+            used[i] = false;
+        }
+    }
+    public static void main(String []args)
+    {
+        int []arr = {1, 2, 3};
+        System.out.println(permut(arr));
+    }
+}
