e9a309544764e72c9fb8a4da05eb47466a77d019
BinayakShome <binayakshome3@gmail.com>
2025-08-31 01:59:02 +0530

    Leetcode problem

---
+++ src/Platform/LeetCode/Medium/TwoSum_InputArraySorted.java
@@ -0,0 +1,18 @@
+package Platform.LeetCode.Medium;
+
+public class TwoSum_InputArraySorted {
+    static int[] twoSum(int []numbers, int target) {
+        int left = 0, right = numbers.length - 1;
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) {
+                return new int[]{left + 1, right + 1};
+            } else if (sum < target) {
+                left++;
+            } else {right--;}}
+        return new int[]{-1, -1};}
+    public static void main(String []args)
+    {int[] numbers = {2,7,11,15};
+        int target = 9;
+        int []ans = twoSum(numbers, target);
+        for(int i = 0; i < ans.length; i++) {System.out.print(ans[i]+"  ");}}}
